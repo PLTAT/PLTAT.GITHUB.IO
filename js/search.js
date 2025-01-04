f92510dbc8d67a1df015c3854741fc96e7a0367a
Rem <2868398105@qq.com>
2025-01-05 07:55:32 +0800

    Initial commit

---
+++ js/search.js
@@ -0,0 +1,32 @@
+
+/*
+JavaScript
+function () { }
+*/
+
+// search.js
+
+/*
+Go
+*/
+
+
+// the html load done
+window.onload = function () {
+    // add a 'keydown' type "listener event" for 'ServerInput' | 'search' is script name
+    document.getElementById('search_input').addEventListener('keydown', function (search) {
+        // 'Enter' key or 'NumpadEnter' key
+        if (search.key === 'Enter' || search.key === 'NumpadEnter') {
+            // ban Default do
+            search.preventDefault();
+            // open window | '_blank' is new tab | '//www.bing.com/search?q=' is link "+ document.getElementById('search_input').value" is add 'search_input' to the end
+            // | // is unsure web link | / is file | some web is 'www.webdnsname' not just 'webdnsname' | some web did't have http jump to https so use // is good
+            window.open('//bing.com/search?q=' + document.getElementById('search_input').value, '_blank');
+        }
+    });
+}
+
+
+/*
+End
+*/
